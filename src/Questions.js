e4923ece8bfc83518d984afeb5e51e4b8732f5b2
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-12-14 09:44:23 -0500

    add

--- src/Questions.js
+++ src/Questions.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react'
 import Post from './component/Post'
 import CommentForm from './component/CommentForm'
-
+import PostForm from './component/PostForm'
 const Questions = () => {
-  const[comments, setComments] =useState([])
+
   //post state for question or any topis to discuss
   const [posts, setPosts] = useState([])
 
   //this function take a comment as a argument
   // take comment and add to gether with other comments in array
-    const handleCommentSubmit=(commentParameter)=>{
-  setComments([...comments,commentParameter])
+    const handleCommentSubmit=(commentParameter,postIndex)=>{
+const updatedPost = [...posts]
+updatedPost[postIndex].comments.push(commentParameter)
+  setPosts(updatedPost)
     }
 
     const handlePostSubmit=(postParameter)=>{
@@ -20,12 +22,16 @@ setPosts([...posts, postParameter])
     return (
       <div>
         <h1>React Post Comments</h1>
-  <Post 
-  title='Sample title Post'
-  content = 'This is Sample Question'
-  comments={comments}
-  />
-  <CommentForm onCommentSubmit={handleCommentSubmit}/>
+  {posts.map((singlePost,index)=>{
+    return(
+<div key={index}>
+<Post title={singlePost.title} content={singlePost.content} comments={singlePost.comments}/>
+<CommentForm onCommentSubmit={(comment)=>handleCommentSubmit(comment,index)}/>
+</div>
+    )
+  })}
+<h2>Create New Post</h2>
+<PostForm onSubmit={handlePostSubmit}/>
       </div>
     )
   }
--- src/component/PostForm.js
+++ src/component/PostForm.js
@@ -1,38 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 //this component do post question
-const PostForm = () => {
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
-    
-    const handleTitleChange=(e)=>{
-setTitle(e.target.value)
-    }
+const PostForm = ({onSubmit}) => {
+    //onSubmit come from question.js line 34--function inside{} from child
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
 
-    const handleContentChange=(e)=>{
-        setContent(e.target.value)
-    }
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const handleSubmit =(e)=>{
+e.preventDefault()
+onSubmit({title,content,comments : []})
+setTitle('')
+setContent('')
+
+  }
 
   return (
     <div>
-        <form>
-<label>
-    Title : 
-    <input type='text' value={title} onChange={handleTitleChange}/>
-</label>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title :
+          <input type="text" value={title} onChange={handleTitleChange} className='' />
+        </label>
 
-<br/>
+        <br />
 
-<label>
-Content : 
-<textarea value={content} onChange={handleContentChange}/>
-
-</label>
-<br/>
-<button type='submit'>Create Post</button>
-
-        </form>
+        <label>
+          Content :
+          <textarea value={content} onChange={handleContentChange} />
+        </label>
+        <br />
+        <button type="submit">Create Post</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default PostForm
+export default PostForm;
